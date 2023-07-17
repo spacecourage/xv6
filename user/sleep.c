0a5718547e9f8bfeaea3b24cb948afd0b739cf35
cyf <chenyufan912@gmail.com>
2023-07-17 16:37:49 +0800

    add sleep

---
+++ user/sleep.c
@@ -0,0 +1,11 @@
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Usage: sleep [time]\n");
+        exit(1);
+    } else {
+        int time = atoi(argv[1]);
+        if (time < 0) exit(1);
+        sleep(time);
+        exit(0);
+    }
+}
