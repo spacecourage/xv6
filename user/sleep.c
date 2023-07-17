4c2b482c89e9fb09155d087c7a1d6ad4680ff890
cyf <chenyufan912@gmail.com>
2023-07-17 17:05:29 +0800

    update

--- user/sleep.c
+++ user/sleep.c
@@ -2,11 +2,12 @@
 #include"kernel/types.h"
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
+    if (argc != 2) {
         printf("Usage: sleep [time]\n");
         exit(1);
     } else {
         int time = atoi(argv[1]);
+        printf("%s\n", argv[0]);
         if (time < 0) exit(1);
         sleep(time);
         exit(0);
