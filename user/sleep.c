9c3ba05a8eeaf807dbd2986361853dac291a0a66
cyf <chenyufan912@gmail.com>
2023-07-17 16:52:01 +0800

    update

--- user/sleep.c
+++ user/sleep.c
@@ -1,3 +1,6 @@
+#include "user/user.h"
+#include"kernel/types.h"
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         printf("Usage: sleep [time]\n");
--- user/user.h
+++ user/user.h
@@ -1,3 +1,4 @@
+#include"kernel/types.h"
 struct stat;
 
 // system calls
